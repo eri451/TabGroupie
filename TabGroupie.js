fbf7520cc7c6b71106a543a96500565262d5a51f
eri451 <hans.orter@gmx.de>
2011-10-27 15:30:05 +0200

    fixed groupchange improvements

--- TabGroupie.js
+++ TabGroupie.js
@@ -115,13 +115,13 @@ let TabGroupie = {
     changeGroup: function changeGroup(TargetGroupTitle){
         let activeTab = window.gBrowser.selectedTab;
         let targetGroupId = this.getIdByTitle(TargetGroupTitle);
-         
+
         function ask(args){
             if (args.length === 0
                 || "" + args[0] === "y"
                 || "" + args[0] === "Y"){ tabs.selectAlternateTab();}
         }
-            
+
         if (targetGroupId != null){
             if (gBrowser.selectedTab._tabViewTabItem.parent._children.length > 1){
                 TabView.moveTabTo(activeTab, targetGroupId);
